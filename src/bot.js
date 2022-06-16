7a74830d03df8ff325d792064b0551964e34aa5f
Alper <61701011+Acapnes@users.noreply.github.com>
2022-06-16 04:16:44 +0300

    Client Commands Handling

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,19 @@
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Interaction, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 require("dotenv").config();
 const fs = require('node:fs');
 const path = require('node:path');
 
 const client = new Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS",
-        "DIRECT_MESSAGE_REACTIONS"
-    ],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.DIRECT_MESSAGES
+	],
 
 });
+
+/// Event Files Imported
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
@@ -25,5 +27,54 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+/// Commands Imported
+
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandsFiles) {
+
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	client.commands.set(command.data.name, command);
+	commands.push(command.data.toJSON());
+}
+
+client.on("ready", () => {
+
+	const guild_ids = client.guilds.cache.map(guild => guild.id);
+
+	const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+	for (const guildId of guild_ids) {
+
+		rest.put(Routes.applicationGuildCommands(process.env.APP_ID, guildId),
+			{ body: commands })
+			.then(() => console.log("Commands updated for guild " + guildId))
+			.catch(err => console.log(err));
+	}
+})
+
+client.on("interaction", async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+
+		await command.execute(interaction);
+
+	} catch (err) {
+		console.log(err);
+		await interaction.reply({ content: "There is an error" });
+	}
+})
+
 
 client.login(process.env.TOKEN);
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'ready',
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+    name: 'ready',
+    execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+    },
 };
