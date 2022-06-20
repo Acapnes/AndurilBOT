97ce49d2eaa71f66022c891b0341e3a83bfa104e
Alper <61701011+Acapnes@users.noreply.github.com>
2022-06-20 23:02:02 +0300

    improved

--- src/commands/getRole.js
+++ src/commands/getRole.js
@@ -1,20 +1,45 @@
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('getrole')
+        .setName('roles')
         .setDescription('Get a role from server!')
-        .addRoleOption(option => option.setName('role').setDescription('Select a role').setRequired(true)),
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('addrole')
+                .setDescription('Add role to user')
+                .addRoleOption(option => option.setName('role').setDescription('Select a role').setRequired(true))
+                .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('removerole')
+                .setDescription('Remove a role from user')
+                .addRoleOption(option => option.setName('role').setDescription('Select a role').setRequired(true))
+                .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))),
     async execute(interaction) {
-        const role = interaction.options.getRole('role');
-        await interaction.member.roles.add(role)
-            .then(async () => {
-                await interaction.reply({ content: "Role added. 🥳" })
-            }).catch(async (err) => {
-                console.err("ROLE ERROR " + err)
-                await interaction.reply({ content: "Something went wrong, please try again. 😔" })
-            });
+
+        if (interaction.options.getSubcommand() === 'addrole') {
+
+            await interaction.options.getMember('user').roles.add(interaction.options.getRole('role'))
+                .then(async () => {
+                    await interaction.reply({ content: "Role added. 🥳" })
+                }).catch(async (err) => {
+                    console.err("ROLE ERROR " + err)
+                    await interaction.reply({ content: "Something went wrong, please try again. 😔" })
+                });
+
+        } else {
+
+            await interaction.options.getMember('user').roles.remove(interaction.options.getRole('role'))
+                .then(async () => {
+                    await interaction.reply({ content: "Role removed. 😬" })
+                }).catch(async (err) => {
+                    console.err("ROLE ERROR " + err)
+                    await interaction.reply({ content: "Something went wrong, please try again. 😔" })
+                });
+
+        }
+
     },
 
 }
