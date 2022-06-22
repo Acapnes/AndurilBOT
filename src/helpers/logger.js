850ab55ed66354c60e7f8b65a67ed55bc818cc70
Alper <61701011+Acapnes@users.noreply.github.com>
2022-06-22 16:56:35 +0300

    Logging editied.

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,19 +1,65 @@
 const fs = require('node:fs');
 
-function KickLogger(memberId, memberName) {
+function KickLogger(memberId, memberName, reason) {
     let json = fs.readFileSync("log.json", "utf-8");
 
     let logs = JSON.parse(json);
     logs["Kicklist"].push({
-        id: memberId,
-        name: memberName
+        memberId,
+        memberName,
+        reason
     });
     json = JSON.stringify(logs);
 
     fs.writeFileSync("log.json", json, "utf-8");
 }
 
+function InviteLogger(memberId, memberName, adminId, adminName) {
+    let json = fs.readFileSync("log.json", "utf-8");
+
+    let logs = JSON.parse(json);
+    logs["Invitelist"].push({
+        memberId,
+        memberName,
+        adminId,
+        adminName
+    });
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync("log.json", json, "utf-8");
+}
+
+function BanLogger(memberId, memberName, days, reason) {
+    let json = fs.readFileSync("log.json", "utf-8");
+
+    let logs = JSON.parse(json);
+    logs["Banlist"].push({
+        memberId,
+        memberName,
+        days,
+        reason
+    });
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync("log.json", json, 'utf-8');
+}
+
+function UnBanLogger(inputId) {
+    let json = fs.readFileSync("log.json", "utf-8");
+
+    let logs = JSON.parse(json);
+
+    for (let i = 0; i < logs["Banlist"].length; i++) { }
+
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync("log.json", json, 'utf-8');
+}
+
 
 module.exports = {
-    KickLogger
+    KickLogger,
+    InviteLogger,
+    BanLogger,
+    UnBanLogger
 }
