23469606c51848dab2842655e381d9f9455bf014
Alper <61701011+Acapnes@users.noreply.github.com>
2022-06-23 12:44:02 +0300

    roll.js

---
+++ src/commands/roll.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("roll")
+        .setDescription("Roll dice.")
+        .addIntegerOption(option => option.setName("start").setDescription("Starts with"))
+        .addIntegerOption(option => option.setName("end").setDescription("Ends with")),
+    async execute(interaction) {
+        await interaction.reply("🧙‍♂️ " + String(Math.floor(Math.random() * (interaction.options.getInteger("end") || 10)) + (interaction.options.getInteger("start") || 0)))
+    }
+}
