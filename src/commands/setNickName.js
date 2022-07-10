10cb045ac9c4a3b55057bf83853dc30a86a44e5c
Alper <61701011+Acapnes@users.noreply.github.com>
2022-07-10 13:03:09 +0300

    Fix setnickname

--- src/commands/setNickName.js
+++ src/commands/setNickName.js
@@ -4,16 +4,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('setnickname')
         .setDescription('Changes your Nickname in this server!')
+        .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
         .addStringOption(option => option.setName('nickname').setDescription('Enter a nickname').setRequired(true)),
-    async execute(client,interaction) {
+    async execute(client, interaction) {
 
         if (!interaction.member.permissions.has("CHANGE_NICKNAME")) return interaction.reply({ content: "Not have enough permission.", ephemeral: true });
 
-        await interaction.member.setNickname(interaction.options.getString('nickname'))
+        await interaction.options.getMember("user").setNickname(interaction.options.getString('nickname'))
             .then(async () => {
                 await interaction.reply({ content: "Your nickname has been changed. 😉" });
             })
             .catch(async (err) => {
+                console.log("SetNickNameError: " + err)
                 await interaction.reply({ content: "Something went wrong. 😕" });
             })
     },
