d65112f16b4299b26f54f4d17be34e9c4ff67b53
Alper <61701011+Acapnes@users.noreply.github.com>
2022-07-17 17:49:36 +0300

    addMember.js v2

--- src/events/addMember.js
+++ src/events/addMember.js
@@ -1,6 +1,16 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: 'guildMemberAdd',
     async execute(client) {
-        client.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID).send(`👋 Welcome! ${client.user}`);
+        console.log(client)
+        let Embed = new MessageEmbed()
+            .setTitle('Welcome to Fellowship! 👋')
+            .setDescription(`${client.user.username}#${client.user.discriminator}`)
+            .setThumbnail(client.user.displayAvatarURL({ format: "png", dynamic: true }))
+            .setColor('RANDOM')
+            .setFooter({ text: `Anduril BOT  🧙‍♂️` })
+        client.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').find(channel => channel.position === 0 && channel.permissionsFor(client.guild.me).has('SEND_MESSAGES')).send({ embeds: [Embed] })
+        // client.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID).send(`👋 Welcome! ${client.user}`);
     },
 };
