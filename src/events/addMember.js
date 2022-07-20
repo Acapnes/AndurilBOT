5995227e275ba0c7ec6047d6e03f17352f084cf7
Alper <61701011+Acapnes@users.noreply.github.com>
2022-07-21 01:35:00 +0300

    need to fix

--- src/events/addMember.js
+++ src/events/addMember.js
@@ -3,13 +3,13 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: 'guildMemberAdd',
     async execute(client) {
-        console.log(client)
-        let Embed = new MessageEmbed()
-            .setTitle('Welcome to Fellowship! 👋')
-            .setDescription(`${client.user.username}#${client.user.discriminator}`)
-            .setThumbnail(client.user.displayAvatarURL({ format: "png", dynamic: true }))
-            .setColor('RANDOM')
-            .setFooter({ text: `Anduril BOT  🧙‍♂️` })
-        client.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').find(channel => channel.position === 0 && channel.permissionsFor(client.guild.me).has('SEND_MESSAGES')).send({ embeds: [Embed] })
+        // console.log(client)
+        // let Embed = new MessageEmbed()
+        //     .setTitle('Welcome to Fellowship! 👋')
+        //     .setDescription(`${client.user.username}#${client.user.discriminator}`)
+        //     .setThumbnail(client.user.displayAvatarURL({ format: "png", dynamic: true }))
+        //     .setColor('RANDOM')
+        //     .setFooter({ text: `Anduril BOT  🧙‍♂️` })
+        // client.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').find(channel => channel.position === 0 && channel.permissionsFor(client.guild.me).has('SEND_MESSAGES')).send({ embeds: [Embed] })
     },
 };
